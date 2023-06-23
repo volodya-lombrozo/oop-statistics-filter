23286cf1de9539f33ab16dd0dd36675012e32de4
volodya-lombrozo <volodya.lombrozo@gmail.com>
2023-06-23 13:11:15 +0300

    Finish with GitHubApplication implementation

--- src/main/java/filter/GitHubApplication.java
+++ src/main/java/filter/GitHubApplication.java
@@ -1,14 +1,13 @@
 package filter;
 
 import java.io.IOException;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.concurrent.TimeUnit;
 
 public class GitHubApplication implements Application {
 
+    private static final int TIMEOUT = 20;
     private final String url;
     private final String tag;
 
@@ -28,7 +27,15 @@ public class GitHubApplication implements Application {
             System.out.println("Absolute file:" + destination.toAbsolutePath());
             Runtime runtime = Runtime.getRuntime();
             final Process process = runtime.exec(this.command(destination));
-            process.waitFor(10, TimeUnit.SECONDS);
+            process.waitFor(GitHubApplication.TIMEOUT, TimeUnit.SECONDS);
+            final int exitValue = process.exitValue();
+            if (exitValue != 0) {
+                throw new IllegalStateException(
+                    String.format("Error while cloning repository, the exit value %s is not 0",
+                        exitValue
+                    )
+                );
+            }
             return destination;
         } catch (IOException ex) {
             throw new IllegalStateException(
