d4a65bdf589cf6d3b19bc738b96bafa452695657
volodya-lombrozo <volodya.lombrozo@gmail.com>
2023-06-26 14:18:20 +0300

    Add Project Reactor, Netty, Jetty statistics

--- src/main/java/filter/FilterApplication.java
+++ src/main/java/filter/FilterApplication.java
@@ -12,82 +12,87 @@ public class FilterApplication {
     public static void main(final String[] args) throws IOException {
         new Report(
 //            new StatisticsCase(
-//                "Apache Derby",
-//                "/Users/lombrozo/Workspace/OpenSource/oop/src/main/profiling/derby/method-list-cpu.csv",
-//                "/Users/lombrozo/Workspace/OpenSource/derby",
+//                "Apache Derby 10.16.1.1",
+//                new RemoteCSV("derby/method-list-cpu.csv"),
+//                new RemoteZipApplication(
+//                    "https://dlcdn.apache.org//db/derby/db-derby-10.16.1.1/db-derby-10.16.1.1-src.zip"
+//                ),
 //                "org.apache.derby"
 //            ),
+//            new StatisticsCase(
+//                "Apache Kafka 3.4.0",
+//                new RemoteCSV("kafka/method-list-cpu.csv"),
+//                new GitHubApplication(
+//                    "https://github.com/apache/kafka.git",
+//                    "3.4.0"
+//                ),
+//                "kafka"
+//            ),
+//            new StatisticsCase(
+//                "Apache Tomcat 10.1.8",
+//                new RemoteCSV("tomcat/method-list-cpu.csv"),
+//                new GitHubApplication(
+//                    "https://github.com/apache/tomcat.git",
+//                    "10.1.8"
+//                ),
+//                "org.apache.tomcat"
+//            ),
+//            new StatisticsCase(
+//                "Spring Framework 5.3.27",
+//                new RemoteCSV("spring-mvc/method-list-cpu.csv"),
+//                new GitHubApplication(
+//                    "https://github.com/spring-projects/spring-framework.git",
+//                    "v5.3.27"
+//                ),
+//                "org.springframework"
+//            ),
+//            new StatisticsCase(
+//                "Takes 1.24.4",
+//                new RemoteCSV("takes/method-list-cpu.csv"),
+//                new GitHubApplication(
+//                    "https://github.com/yegor256/takes.git",
+//                    "1.24.4"
+//                ),
+//                "org.takes"
+//            ),
+//            new StatisticsCase(
+//                "Verification Half",
+//                new RemoteCSV("verification-half/method-list-cpu.csv"),
+//                new GitHubApplication(
+//                    "https://github.com/volodya-lombrozo/cost-of-oop.git",
+//                    "main"
+//                ),
+//                "verification.half"
+//            ),
+//            new StatisticsCase(
+//                "Verification Instance",
+//                new RemoteCSV("verification-instance/method-list-cpu.csv"),
+//                new GitHubApplication(
+//                    "https://github.com/volodya-lombrozo/cost-of-oop.git",
+//                    "main"
+//                ),
+//                "verification.instances"
+//            ),
+//            new StatisticsCase(
+//                "Verification Statics",
+//                new RemoteCSV("verification-static/method-list-cpu.csv"),
+//                new GitHubApplication(
+//                    "https://github.com/volodya-lombrozo/cost-of-oop.git",
+//                    "main"
+//                ),
+//                "verification.statics"
+//            ),
+
             new StatisticsCase(
-                "Apache Derby 10.16.1.1",
-                new RemoteCSV("derby/method-list-cpu.csv"),
-                new RemoteZipApplication(
-                    "https://dlcdn.apache.org//db/derby/db-derby-10.16.1.1/db-derby-10.16.1.1-src.zip"
-                ),
-                "org.apache.derby"
-            ),
-            new StatisticsCase(
-                "Apache Kafka 3.4.0",
-                new RemoteCSV("kafka/method-list-cpu.csv"),
+                "Jetty 10.0.15",
+                new RemoteCSV("struts/method-list-cpu.csv"),
                 new GitHubApplication(
-                    "https://github.com/apache/kafka.git",
-                    "3.4.0"
+                    "https://github.com/eclipse/jetty.project.git",
+                    "jetty-10.0.15"
                 ),
-                "kafka"
+                "org.eclipse.jetty"
             ),
-            new StatisticsCase(
-                "Apache Tomcat 10.1.8",
-                new RemoteCSV("tomcat/method-list-cpu.csv"),
-                new GitHubApplication(
-                    "https://github.com/apache/tomcat.git",
-                    "10.1.8"
-                ),
-                "org.apache.tomcat"
-            ),
-            new StatisticsCase(
-                "Spring Framework 5.3.27",
-                new RemoteCSV("spring-mvc/method-list-cpu.csv"),
-                new GitHubApplication(
-                    "https://github.com/spring-projects/spring-framework.git",
-                    "v5.3.27"
-                ),
-                "org.springframework"
-            ),
-            new StatisticsCase(
-                "Takes 1.24.4",
-                new RemoteCSV("takes/method-list-cpu.csv"),
-                new GitHubApplication(
-                    "https://github.com/yegor256/takes.git",
-                    "1.24.4"
-                ),
-                "org.takes"
-            ),
-            new StatisticsCase(
-                "Verification Half",
-                new RemoteCSV("verification-half/method-list-cpu.csv"),
-                new GitHubApplication(
-                    "https://github.com/volodya-lombrozo/cost-of-oop.git",
-                    "main"
-                ),
-                "verification.half"
-            ),
-            new StatisticsCase(
-                "Verification Instance",
-                new RemoteCSV("verification-instance/method-list-cpu.csv"),
-                new GitHubApplication(
-                    "https://github.com/volodya-lombrozo/cost-of-oop.git",
-                    "main"
-                ),
-                "verification.instances"
-            ),
-            new StatisticsCase(
-                "Verification Statics",
-                new RemoteCSV("verification-static/method-list-cpu.csv"),
-                new GitHubApplication(
-                    "https://github.com/volodya-lombrozo/cost-of-oop.git",
-                    "main"
-                ),
-                "verification.statics"
-            ),
+
             new StatisticsCase(
                 "Struts 6.1.2",
                 new RemoteCSV("struts/method-list-cpu.csv"),
@@ -96,16 +101,37 @@ public class FilterApplication {
                     "STRUTS_6_1_2"
                 ),
                 "org.apache.struts2"
-            ),
-            new StatisticsCase(
-                "Micronaut 3.9.3",
-                new RemoteCSV("micronaut/method-list-cpu.csv"),
-                new GitHubApplication(
-                    "https://github.com/micronaut-projects/micronaut-core.git",
-                    "v3.9.3"
-                ),
-                "io.micronaut"
             )
+//            ,
+
+//
+//            new StatisticsCase(
+//                "Project Reactor 3.5.0",
+//                new RemoteCSV("micronaut/method-list-cpu.csv"),
+//                new GitHubApplication(
+//                    "https://github.com/reactor/reactor-core.git",
+//                    "v3.5.0"
+//                ),
+//                "reactor.core"
+//            ),
+//            new StatisticsCase(
+//                "Netty 4.1.92.Final",
+//                new RemoteCSV("micronaut/method-list-cpu.csv"),
+//                new GitHubApplication(
+//                    "https://github.com/netty/netty.git",
+//                    "netty-4.1.92.Final"
+//                ),
+//                "io.netty"
+//            ),
+//            new StatisticsCase(
+//                "Micronaut 3.9.3",
+//                new RemoteCSV("micronaut/method-list-cpu.csv"),
+//                new GitHubApplication(
+//                    "https://github.com/micronaut-projects/micronaut-core.git",
+//                    "v3.9.3"
+//                ),
+//                "io.micronaut"
+//            )
         ).make();
     }
 
